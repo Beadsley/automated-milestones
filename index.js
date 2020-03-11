c61c8bad4b70ceeb4da9e22ac4238281a6f21419
beadsley <danbeadleson@gmail.com>
2020-03-11 20:03:23 +0100

    push webhook added, that adds a status to a commit message

--- index.js
+++ index.js
@@ -5,6 +5,26 @@
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
+  const ACCESSTOKEN=process.env.TOKEN;
+
+  app.on('push', async context => {
+    app.log.info('push open:')
+    app.log.info(context.payload.after);
+    app.log.info('REPO: ', context.payload.repository.name);
+    app.log.info('ID: ', context.payload.commits[0].id);
+    app.log.info('Message: ', context.payload.head_commit.message);
+
+    const params = {
+      sha: context.payload.commits[0].id,
+      "target_url": `https://github.com/Beadsley/Simple-Website-using-HTML-CSS-and-Javascript-/milestone/5`,
+      context: 'milestone',
+      state: 'success' ,
+      description: `wubba dub dub`
+    }
+    return context.github.repos.createStatus(context.repo(params));
+    // const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    // return context.github.issues.createComment(issueComment)
+  })
 
   app.on('issues.opened', async context => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
