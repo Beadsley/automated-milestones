7044ded25092f0753a3afabeffa174f7b74e199b
beadsley <danbeadleson@gmail.com>
2020-03-12 07:12:23 +0100

    added eventlisteners to all the info buttons

---
+++ client/public/index.js
@@ -0,0 +1,23 @@
+
+const infoButtons = document.querySelectorAll('#info');
+infoButtons.forEach(info => {
+    info.addEventListener('click', () => {
+        console.log('clicked');
+    
+    });
+    
+    info.addEventListener('mouseover', () => {
+        console.log('hover no');
+        document.body.style.cursor = 'pointer';
+    
+    });
+    
+    info.addEventListener('mouseleave', () => {
+        console.log('hover');
+        document.body.style.cursor = 'default';
+    
+    });
+});
+
+
+
