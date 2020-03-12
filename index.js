e8ca8b1c1df161f16e80fb430cdf220e7c6482c4
beadsley <danbeadleson@gmail.com>
2020-03-12 15:10:14 +0100

    successfully closes a milestone from a commit message

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ let ACCESSTOKEN;
  */
 module.exports = app => {
   ACCESSTOKEN = process.env.TOKEN;
-  commitMessages();
+
+
   router.use(express.static('client/public'))
   router.listen(8080, async () => {
     console.log('http://localhost:8080/');
@@ -36,15 +37,21 @@ module.exports = app => {
     app.log.info('REPO: ', context.payload.repository.name);
     app.log.info('ID: ', context.payload.commits[0].id);
     app.log.info('Message: ', context.payload.head_commit.message);
-    const repo=context.payload.repository.name;
-    const commitmessage=context.payload.head_commit.message;
+    const repo = context.payload.repository.name;
+    const commitmessage = context.payload.head_commit.message;
+    const milestoneids = await commitMessages(commitmessage);
+    app.log.info('milestone: ', milestoneids[0]);
+
+    const milestone = await getMilestone(milestoneids[0]);
+
+    updateMilestone(milestone.id, milestone.title, milestone.description, milestone.due_on);
 
     const params = {
       sha: context.payload.commits[0].id,
-      "target_url": `https://github.com/Beadsley/Simple-Website-using-HTML-CSS-and-Javascript-/milestone/5`,
+      "target_url": `${milestone.html_url}`,
       context: 'milestone',
       state: 'success',
-      description: `wubba dub dub`
+      description: `${milestone.title}`
     }
     return context.github.repos.createStatus(context.repo(params));
     // const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
@@ -89,7 +96,7 @@ const getMilestones = async () => {
   const response = await fetchFromGithubAPI(`/repos/${USER}/${REPO}/milestones?state=all`);
   const currentDate = new Date();
   console.log(response);
-  
+
   const milestones = response.data.map(milestone => {
     const dueDate = Date.parse(milestone.due_on);
     const formattedDate = moment(dueDate).format("MMM Do");
@@ -107,49 +114,49 @@ const getMilestones = async () => {
   return milestones;
 }
 
-const commitMessages = async () => {
-  const commit = "completes #4  completes #5"
+const commitMessages = async (commit) => {
   const regex = /completes\s*\#\d+/ig;
-  
-      let m;
-      while ((m = regex.exec(commit)) !== null) {
-          // This is necessary to avoid infinite loops with zero-width matches
-          if (m.index === regex.lastIndex) {
-              regex.lastIndex++;
-          }
-          // The result can be accessed through the `m`-variable.
-          m.forEach(async (match, groupIndex) => {
-              console.log(`Found match, group ${groupIndex}: ${match}`);
-              const id = /\d+/.exec(match)[0];
-              console.log(`Milestone number: ${id}`);
-              const milestone = await getMilestone(id);
-              updateMilestone(milestone.id, milestone.title, milestone.description, milestone.due_on);
-          });
-      }
+
+  let m;
+  let result = [];
+  while ((m = regex.exec(commit)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+    // The result can be accessed through the `m`-variable.
+    m.forEach(async (match, groupIndex) => {
+      console.log(`Found match, group ${groupIndex}: ${match}`);
+      const id = /\d+/.exec(match)[0];
+      result.push(id);
+      console.log(`Milestone number: ${id}`);
+    });
+  }
+  return result;
 }
 const getMilestone = async (id) => {
   const github = await fetchFromGithubAPI(`/repos/${USER}/${REPO}/milestones/${id}`);
   return milestone = {
-      "id": github.data.number,
-      "title": github.data.title,
-      "description": github.data.description,
-      "due_on": github.data.due_on
+    "id": github.data.number,
+    "title": github.data.title,
+    "description": github.data.description,
+    "due_on": github.data.due_on
   }
 }
 
 const updateMilestone = (id, title, description, due) => {
   fetch(`https://api.github.com/repos/${USER}/${REPO}/milestones/${id}`, {
-      method: 'patch',
-      headers: {
-          "Content-Type": "application/json",
-          "Authorization": `token ${ACCESSTOKEN}`
-      },
-      body: JSON.stringify({
-          "title": title,
-          "state": "closed",
-          "description": description,
-          "due_on": due
-      })
+    method: 'patch',
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `token ${ACCESSTOKEN}`
+    },
+    body: JSON.stringify({
+      "title": title,
+      "state": "closed",
+      "description": description,
+      "due_on": due
+    })
   }).then(response => response.json()).then(json => console.log(json));
 }
 
